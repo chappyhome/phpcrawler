8c880bfbd94c284dedb35c8d417631c9d7a3819b
ilei <wlm131127@163.com>
2015-07-25 21:47:41 +0800

    添加文件注释

--- ThinkCrawler/constants.php
+++ ThinkCrawler/constants.php
@@ -1,4 +1,15 @@
 <?php 
+// +----------------------------------------------------------------------
+// | ThinkCrawler Framework [ I CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2011-2015 ThinkLei Team (http://www.smartlei.com)
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author ThinkLei <ThinkLei@126.com>
+// +----------------------------------------------------------------------
+
+//定义一些常量
 define('APP_PATH'   ,  __DIR__);
 define('CORE_PATH'  ,  APP_PATH  . '/system/');
 define('HELPER_PATH',  CORE_PATH . 'helper/');
--- ThinkCrawler/start_parse.php
+++ ThinkCrawler/start_parse.php
@@ -1,4 +1,14 @@
 <?php 
+// +----------------------------------------------------------------------
+// | ThinkCrawler Framework [ I CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2011-2015 ThinkLei Team (http://www.smartlei.com)
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author ThinkLei <ThinkLei@126.com>
+// +----------------------------------------------------------------------
+
 include 'constants.php';
 include CORE_PATH. 'Loader.php';
 Loader::load('helpers.dom');
